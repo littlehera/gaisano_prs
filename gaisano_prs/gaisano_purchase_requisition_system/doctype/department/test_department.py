1f504f03f57ea2e1b94d7f229997af5338fb0e7e
Herabelle Villanueva <villanuevahera@gmail.com>
2025-12-24 22:53:06 +0800

    First Push

---
+++ gaisano_prs/gaisano_purchase_requisition_system/doctype/department/test_department.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, hvillanueva and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDepartment(FrappeTestCase):
+	pass
