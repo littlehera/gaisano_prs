a7b0c497a7e48ed297b89a60606f88c76d234e0b
Herabelle Villanueva <villanuevahera@gmail.com>
2026-01-30 14:08:13 +0800

    added feature to check if final order was changed

--- gaisano_prs/gaisano_purchase_requisition_system/doctype/purchase_requisition/purchase_requisition.py
+++ gaisano_prs/gaisano_purchase_requisition_system/doctype/purchase_requisition/purchase_requisition.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2025, hvillanueva and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class PurchaseRequisition(Document):
+	def validate(self):
+		items = self.items
+		for item in items:
+			calculated_order = item.order_qty - item.inv_onhand if item.order_qty >= item.inv_onhand else 0
+			if item.final_order != calculated_order:
+				item.final_order_changed = 1
+				frappe.msgprint(f"Final Order for item {item.item_description} does not match calculated order.")
+			else:
+				item.final_order_changed = 0
 	pass
